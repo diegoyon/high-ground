1f5be5e2afec35109ab84c7d656e4e69f01c0895
Diego <diegoyon@outlook.com>
2023-08-24 16:06:28 -0600

    prepare webhook for updates

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,18 @@
+class WebhooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def fri
+    payload = JSON.parse(request.body.read)
+    matching_athlete = Athlete.find_by(payment_request_id: payload['paymentRequestId'], reference: payload['reference'])
+
+    if matching_athlete
+      matching_athlete.payment_status = payload['status']
+      matching_athlete.save
+    else
+      p '###########################'
+      p 'No matching athlete found'
+    end
+
+    render json: { message: 'ok' }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :athletes
   root "home#index"
   get "/success", to: "home#success"
-  post 'webhooks/fri', to: 'webhooks#fri
+  post "webhooks/fri", to: "webhooks#fri"
 end
