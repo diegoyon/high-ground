ba380126d38cf5560daa0eeb711daa24f428a82d
Diego <diegoyon@outlook.com>
2023-08-25 14:04:54 -0600

    Include blank in the tshirt size

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,7 +1,9 @@
 class Athlete < ApplicationRecord
-  validates :first_name, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :last_name, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { minimum: 2, maximum: 80 }
-  validates :phone, presence: true, length: { minimum: 2, maximum: 20 }
-  validates :fri_username, presence: true, length: { minimum: 1, maximum: 50 }
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { maximum: 50 }
+  validates :phone, presence: true, length: { maximum: 20 }
+  validates :fri_username, presence: true, length: { maximum: 50 }
+  validates :tshirt_size, presence: true
+  validates :box, presence: true, length: { maximum: 50 }
 end
