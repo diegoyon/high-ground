80fbf49658d38400d3d9f933c66528ee0b84d3de
Diego <diegoyon@outlook.com>
2023-08-27 23:23:54 -0600

    add price card and change credentials

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -88,8 +88,9 @@ class AthletesController < ApplicationController
         }.to_json,
         headers: { 'Content-Type' => 'application/json' }
       })
-
+      p "#############"
       api_data = JSON.parse(api_response_login.body)
+      p api_data
       sessionId = api_data['responseContent']['sessionId']
 
       # Request payment
