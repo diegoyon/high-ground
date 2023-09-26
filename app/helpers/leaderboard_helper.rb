cc1237bc60ccf8c9cdff274e0f8365c8d411b2ac
Diego <diegoyon@outlook.com>
2023-09-26 09:21:56 -0600

    show correct time when editing

--- app/helpers/leaderboard_helper.rb
+++ app/helpers/leaderboard_helper.rb
@@ -3,14 +3,6 @@ module LeaderboardHelper
     @workouts.count + 2
   end
 
-  def format_time(seconds)
-    # duration = ActiveSupport::Duration.build(seconds)
-    # duration.parts.map { |part| part.last.to_s.rjust(2, '0') }.join(':')
-    minutes = seconds / 60
-    seconds = seconds % 60
-    formatted_time = format('%02d:%02d', minutes, seconds)
-  end
-
   def format_score(score, measurement_type)
     case measurement_type
     when "AMRAP"
