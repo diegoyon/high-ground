f680e2e25ed5227db56e3e7296ff1a3d4606d243
Diego <diegoyon@outlook.com>
2023-10-02 10:52:11 -0600

    add filters

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,18 +1,12 @@
 class LeaderboardController < ApplicationController
   def index
     @athletes = Athlete.ready.order(:rank)
-    search_athletes
     filter_athletes
     @workouts = Workout.order(:workout_number)
   end
 
   private
-
-  def search_athletes
-    return unless params[:query].present?
-    @athletes = @athletes.where('lower(first_name || \' \' || last_name) LIKE :query', query: "%#{params[:query].downcase}%")
-  end
-
+  
   def filter_athletes
     if params[:division].present?
       @athletes = @athletes.where(division: params[:division])
