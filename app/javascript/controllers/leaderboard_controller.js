841349d348952a99716f785add78c8d4386bf58a
Diego <diegoyon@outlook.com>
2023-10-02 10:33:02 -0600

    finish styling dropdown leadearboard

--- app/javascript/controllers/leaderboard_controller.js
+++ app/javascript/controllers/leaderboard_controller.js
@@ -1,39 +1,47 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    // Check the screen size when the controller is connected
-    this.checkScreenSize();
+  static targets = ['iconDown', 'iconUp'];
+  // connect() {
+  //   // Check the screen size when the controller is connected
+  //   this.checkScreenSize();
 
-    // Add a resize event listener to check screen size changes
-    window.addEventListener('resize', this.checkScreenSize.bind(this));
-  }
+  //   // Add a resize event listener to check screen size changes
+  //   window.addEventListener('resize', this.checkScreenSize.bind(this));
+  // }
 
-  checkScreenSize() {
-    // Get the current window width
-    const windowWidth = window.innerWidth;
+  // checkScreenSize() {
+  //   // Get the current window width
+  //   const windowWidth = window.innerWidth;
 
-    // Define the screen size threshold for enabling the controller
-    const screenSizeThreshold = 768; // For example, 768 pixels for mobile view
+  //   // Define the screen size threshold for enabling the controller
+  //   const screenSizeThreshold = 768; // For example, 768 pixels for mobile view
 
-    if (windowWidth <= screenSizeThreshold) {
-      // Enable the controller and add the toggle listener
-      this.isActive = true;
-    } else if (windowWidth > screenSizeThreshold) {
-      // Disable the controller and remove the toggle listener
-      this.isActive = false;
-    }
-  }
+  //   if (windowWidth <= screenSizeThreshold) {
+  //     // Enable the controller and add the toggle listener
+  //     this.isActive = true;
+  //   } else if (windowWidth > screenSizeThreshold) {
+  //     // Disable the controller and remove the toggle listener
+  //     this.isActive = false;
+  //   }
+  // }
 
 
   toggle() {
-    if (this.isActive) {
-      let elementId = `${this.element.id}_details`
-      let athleteDetails = document.getElementById(elementId)
-  
-      let isClose = athleteDetails.classList.contains('max-h-0')
-      isClose ? this.show(athleteDetails) : this.hide(athleteDetails)
-    }
+    // if (this.isActive) {
+    let elementId = `${this.element.id}_details`
+    let athleteDetails = document.getElementById(elementId)
+
+    let isClose = athleteDetails.classList.contains('max-h-0')
+    isClose ? this.show(athleteDetails) : this.hide(athleteDetails)
+
+    const iconDown = this.iconDownTarget;
+    const iconUp = this.iconUpTarget;
+
+    // Toggle the visibility of the icons
+    iconDown.classList.toggle('hidden');
+    iconUp.classList.toggle('hidden');
+    // }
   }
 
   show(athleteDetails) {
