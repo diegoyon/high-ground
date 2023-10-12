e8284ec91688f2c5f9cdf05b5851d25c8975015e
Diego <diegoyon@outlook.com>
2023-10-11 21:15:14 -0600

    add sign up page again

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   resources :scores
 
   resources :athletes, only: [:index, :show]
-  # resources :fri_athletes, only: [:new, :create]
-  # resources :recurrente_athletes, only: [:new, :create]
+  resources :fri_athletes, only: [:new, :create]
+  resources :recurrente_athletes, only: [:new, :create]
 
   root "home#index"
 
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get "/payment_completed", to: "home#payment_completed"
   get "/payment_failed", to: "home#payment_failed"
 
-  # get "/payment_options", to: "home#payment_options"
+  get "/payment_options", to: "home#payment_options"
   post "webhooks/fri", to: "webhooks#fri"
   post "webhooks/recurrente", to: "webhooks#recurrente"
 
