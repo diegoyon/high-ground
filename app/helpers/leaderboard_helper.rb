6e30af578be8fcfae8dfb8d2fc3ea83e1308c399
Diego <diegoyon@outlook.com>
2023-10-13 15:19:21 -0600

    fix leaderboard helpers linters

--- app/helpers/leaderboard_helper.rb
+++ app/helpers/leaderboard_helper.rb
@@ -1,18 +1,35 @@
 # frozen_string_literal: true
 
 module LeaderboardHelper
-  def format_score(score, measurement_type, time_cap = nil)
-    case measurement_type
+  def format_score(score)
+    case score.workout_type
     when 'AMRAP'
-      "#{score} reps"
+      "#{score.main_score} reps"
     when 'Weight'
-      "#{score} lbs"
+      "#{score.main_score} lbs"
     when 'Time'
-      if time_cap.present? && score > time_cap
-        "CAP + #{score - time_cap}"
-      else
-        format_time(score)
-      end
+      time_or_capped(score)
+    else
+      ''
+    end
+  end
+
+  def time_or_capped(score)
+    if score.capped
+      "CAP + #{score.main_score - score.time_cap}"
+    else
+      format_time(score.main_score)
+    end
+  end
+
+  def format_tiebreak_score(score)
+    case score.tiebreak_type
+    when 'AMRAP'
+      "#{score.tiebreak_score} reps"
+    when 'Weight'
+      "#{score.tiebreak_score} lbs"
+    when 'Time'
+      format_time(score.tiebreak_score)
     else
       ''
     end
