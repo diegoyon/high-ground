599dd6d5783f74e2a28a132b1b576ecf5e8840f0
Diego <diegoyon@outlook.com>
2023-10-31 12:13:41 -0600

    add dropdown for workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,6 +2,12 @@
 
 class WorkoutsController < ApplicationController
   def index
-    @workout = Workout.find(params[:workout_id].presence || 1)
+    @workout = Workout.visible.find_by(workout_number: params[:workout_number].presence) || default_workout
+  end
+
+  private
+
+  def default_workout
+    Workout.visible.order(workout_number: :asc).first || nil
   end
 end
