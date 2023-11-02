b38b4864caeffd84ed85453decbaa04268fd9d53
Diego <diegoyon@outlook.com>
2023-11-01 22:41:59 -0600

    add select element for divisions

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,12 +2,26 @@
 
 class WorkoutsController < ApplicationController
   def index
-    @workout = Workout.visible.find_by(workout_number: params[:workout_number].presence) || default_workout
+    @workout = selected_workout || default_workout
+    @divisions = selected_workout&.divisions || default_divisions
+    @division = selected_division || @workout.divisions.first
   end
 
   private
 
+  def selected_workout
+    Workout.visible.find_by(workout_number: params[:workout_number].presence)
+  end
+
   def default_workout
     Workout.visible.order(workout_number: :asc).first || nil
+  end
+
+  def default_divisions
+    Workout.visible.order(workout_number: :asc).first.divisions || nil
+  end
+
+  def selected_division
+    Division.find(params[:division_id]) if params[:division_id].present?
   end
 end
