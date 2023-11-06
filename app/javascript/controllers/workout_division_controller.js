f965df80a9215bd357c9e08c31c2b1caac9dd4c7
Diego <diegoyon@outlook.com>
2023-11-06 10:42:24 -0600

    remove printing division

--- app/javascript/controllers/workout_division_controller.js
+++ app/javascript/controllers/workout_division_controller.js
@@ -3,9 +3,7 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
   static targets = ['workoutSelect']
 
-  submit(event) {
+  submit() {
     this.element.requestSubmit();
-    console.log(this.workoutSelectTarget.value);
-    this.workoutSelectTarget.value = `${this.workoutSelectTarget.value}`;
   }
 }
